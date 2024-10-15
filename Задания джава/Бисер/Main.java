68e6496c2cf72713198a3b902b4c26b92b1e8c97
Dix1k <Dix1k@yandex.ru>
2024-10-15 17:27:59 +0500

    Zadania(1-3)

---
+++ Задания джава/Бисер/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        scanner.close();
+
+        int result = N + 1;
+
+        System.out.println(result);
+    }
+}
