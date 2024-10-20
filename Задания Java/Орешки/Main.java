6e3789158eb73971ed960fc48f2f6bd5b14a2949
Dix1k <Dix1k@yandex.ru>
2024-10-20 17:30:02 +0500

    Zadanie-6

---
+++ Задания Java/Орешки/Main.java
@@ -0,0 +1,27 @@
+//https://acmp.ru/index.asp?main=task&id_task=766s
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        scanner.close();
+
+        String[] numbers = input.split(" ");
+        
+        int A = Integer.parseInt(numbers[0]);
+        int B = Integer.parseInt(numbers[1]);
+        int C = Integer.parseInt(numbers[2]);
+
+        int nuts = (A * B) - C;
+
+        if (nuts >= 0) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
