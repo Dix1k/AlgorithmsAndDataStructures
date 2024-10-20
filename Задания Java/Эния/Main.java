0c96080a4612897714ea2cff57ffcd9643abab08
Dix1k <Dix1k@yandex.ru>
2024-10-20 17:57:37 +0500

    Zadanie-7

---
+++ Задания Java/Эния/Main.java
@@ -0,0 +1,22 @@
+//https://acmp.ru/index.asp?main=task&id_task=766s
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        scanner.close();
+
+        String[] numbers = input.split(" ");
+        
+        int N = Integer.parseInt(numbers[0]);
+        int A = Integer.parseInt(numbers[1]);
+        int B = Integer.parseInt(numbers[2]);
+
+        int res = (2 * N) * (A * B);
+
+        System.out.println(res);
+        }
+    }
